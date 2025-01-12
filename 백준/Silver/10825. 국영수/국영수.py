b11d0adedcb435bb64d2285e1b0d22d577f5ca97
MinJae <80663462+Dlawoct@users.noreply.github.com>
2025-01-12 16:39:08 +0900

    [Silver IV] Title: 국영수, Time: 384 ms, Memory: 83912 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input().strip())
+students = [input().strip().split() for _ in range(N)]
+
+students.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for student in students:
+    print(student[0])
