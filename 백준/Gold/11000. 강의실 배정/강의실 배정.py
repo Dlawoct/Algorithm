9d352d8093b60f11e553c308133a03b647f5cc0e
MinJae <80663462+Dlawoct@users.noreply.github.com>
2025-01-13 21:27:25 +0900

    [Gold V] Title: 강의실 배정, Time: 344 ms, Memory: 75524 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,24 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+N = int(input())
+
+times = [list(map(int, input().split())) for _ in range(N)]
+times.sort(key= lambda x : x[0])
+
+# 시작 시간 S, 종료 시간 T
+# 1) 다음의 S가 T보다 같거나 크면(즉, 시간이 나중이면) 수업을 같은 강의실에서 들을 수 있음 -> 한 수업으로 쳐도 될거 같음
+# 2) 다음의 S가 T보다 작으면(즉, 시간이 이전이면) 다른 강의실 하나를 빌려야 됨
+
+heap = []
+heapq.heappush(heap, times[0][1])  # 처음 강의 종료 시간
+
+for i in range(1, N):
+    if times[i][0] >= heap[0]:  # 1)으로 같은 강의실 취급이므로 pop
+        heapq.heappop(heap)
+
+    heapq.heappush(heap, times[i][1])
+
+# 힙의 크기가 필요한 강의실 수
+print(len(heap))
